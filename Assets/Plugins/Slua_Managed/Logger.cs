da58d9e9a45c2526a617e6095e533fd743f6f92e
mr-kelly <chepy.v@gmail.com>
2017-03-06 14:25:05 +0800

    Slua Logger log callback

--- Assets/Plugins/Slua_Managed/Logger.cs
+++ Assets/Plugins/Slua_Managed/Logger.cs
@@ -31,17 +31,37 @@ namespace SLua
 #endif
             return null;
         }
+        public enum Level
+        {
+            Debug,
+            Warning,
+            Error
+        }
+        public static Action<Level, string> LogAction;
 
         public static void Log(string msg)
         {
+            if (LogAction != null)
+            {
+                LogAction(Level.Debug, msg);
+                return;
+            }
+
 #if !SLUA_STANDALONE
             UnityEngine.Debug.Log(msg);
 #else
             Console.WriteLine(msg);
 #endif 
         }
+
         public static void LogError(string msg, bool hasStacktrace = false)
         {
+            if (LogAction != null)
+            {
+                LogAction(Level.Error, msg);
+                return;
+            }
+
 #if !SLUA_STANDALONE
             if(hasStacktrace)
             {
@@ -60,6 +80,12 @@ namespace SLua
 
 		public static void LogWarning(string msg)
 		{
+            if (LogAction != null)
+            {
+                LogAction(Level.Warning, msg);
+                return;
+            }
+
 #if !SLUA_STANDALONE
 			UnityEngine.Debug.LogWarning(msg);
 #else
