c4a83b428238ba793f43eed75db01d8bf4e65c4e
Pang Weiwei <sineysan@163.com>
2017-04-27 17:04:58 +0800

    luatable can set negative/zeor int as key

--- Assets/Plugins/Slua_Managed/LuaState.cs
+++ Assets/Plugins/Slua_Managed/LuaState.cs
@@ -1160,19 +1160,17 @@ end
 		internal object getObject(int reference, int index)
 		{
 			if (index >= 1) {
-				int oldTop = LuaDLL.lua_gettop (L);
 				LuaDLL.lua_getref (L, reference);
 				LuaDLL.lua_rawgeti (L, -1, index);
 				object returnValue = getObject (L, -1);
-				LuaDLL.lua_settop (L, oldTop);
+				LuaDLL.lua_pop (L, 1);
 				return returnValue;
 			} else {
-				int oldTop = LuaDLL.lua_gettop (L);
 				LuaDLL.lua_getref (L, reference);
 				LuaDLL.lua_pushinteger (L, index);
 				LuaDLL.lua_gettable (L, -2);
 				object returnValue = getObject (L, -1);
-				LuaDLL.lua_settop (L, oldTop);
+				LuaDLL.lua_pop (L, 1);
 				return returnValue;
 			}
 		}
